49d1f55ba7eb8995c23aa276d5c2dc83ac920e18
mogstuff <markmorgan.mcp@gmail.com>
2018-09-07 21:48:51 +0100

    - initial commit

--- PhoneWord/MainActivity.cs
+++ PhoneWord/MainActivity.cs
@@ -1,10 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Runtime;
+
+// using Android.Support.V7.App;
+
 
 namespace PhoneWord
 {
     [Activity(Label = "PhoneWord", MainLauncher = true)]
+//    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -13,6 +18,28 @@ namespace PhoneWord
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+
+
         }
     }
 }
